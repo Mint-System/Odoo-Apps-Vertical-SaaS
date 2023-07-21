9bc182cd6c0e05fdc683876ed427b471b12c31ea
Janik von Rotz <login@janikvonrotz.ch>
2023-07-21 15:16:45 +0200

    [license] upload

--- license/models/license_license.py
+++ license/models/license_license.py
@@ -1,7 +1,8 @@
-from odoo import _, api, fields, models
 import logging
-_logger = logging.getLogger(__name__)
 import uuid
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+_logger = logging.getLogger(__name__)
 
 
 class License(models.Model):
@@ -9,10 +10,10 @@ class License(models.Model):
     _name = 'license.license'
     _description = 'License'
 
-    name = fields.Char(default=lambda self: _('New'), readonly=True, states={'draft': [('readonly', False)], 'assigned': [('readonly', False)]})
-    key = fields.Char(compute='_compute_key', tracking=True, store=True, states={'draft': [('readonly', False)], 'assigned': [('readonly', False)]})
+    name = fields.Char(default=lambda self: _('New'), required=True, readonly=True, states={'draft': [('readonly', False)], 'assigned': [('readonly', False)]})
+    key = fields.Char(default=lambda self: _('New'), compute='_compute_key', tracking=True, required=True, store=True, states={'draft': [('readonly', False)], 'assigned': [('readonly', False)]})
     type_id = fields.Many2one('license.type', readonly=True, states={'draft': [('readonly', False)], 'assigned': [('readonly', False)]})
-    partner_id = fields.Many2one('res.partner', tracking=True, readonly=True, states={'draft': [('readonly', False)], 'assigned': [('readonly', False)]})
+    partner_id = fields.Many2one('res.partner', tracking=True, readonly=True, states={'draft': [('readonly', False)]})
     product_id = fields.Many2one('product.product', tracking=True, readonly=True, states={'draft': [('readonly', False)], 'assigned': [('readonly', False)]})
     state = fields.Selection(
         selection=[
@@ -39,9 +40,62 @@ class License(models.Model):
 
     @api.depends('create_date')
     def _compute_key(self):
-        for rec in self.filtered(lambda l: not l.key):
-            rec.key = str(uuid.uuid4()).upper()
+        for license in self.filtered(lambda l: l.key == _('New')):
+            license.key = str(uuid.uuid4()).upper()
 
-    def assign(self):
-        for rec in self:
-            rec.write({'state': 'assigned'})
+    def action_assign(self):
+        for license in self:
+            if not license.partner_id:
+                raise UserError(
+                    _("You cannot assign a license if no partner is set.")
+                )
+            license.write({'state': 'assigned'})
+
+    def action_activate(self):
+        for license in self:
+            license.write({
+                'state': 'active',
+                'date_start': fields.Datetime.now()
+            })
+
+    def action_reset(self):
+        for license in self:
+            license.write({
+                'state': 'assigned',
+                'date_start': False,
+                'date_end': False
+            })
+
+    def action_disable(self):
+        for license in self:
+            license.write({
+                'state': 'disabled',
+                'date_end': fields.Datetime.now()
+            })
+
+    def action_cancel(self):
+        for license in self:
+            license.write(
+                {'state': 'cancelled',
+                'date_end': fields.Datetime.now()
+            })
+
+    def action_draft(self):
+        for license in self:
+            license.write({
+                'state': 'draft',
+                'date_start': False,
+                'date_end': False
+            })
+
+    def _can_be_deleted(self):
+        self.ensure_one()
+        return self.state in ['draft', 'cancelled']
+
+    def unlink(self):
+        for license in self:
+            if not license._can_be_deleted():
+                raise UserError(
+                    _("You cannot delete a licnese which is not draft or cancelled.")
+                )
+        return super(License, self).unlink()
