f128b9f788ef1f161b26b1cabbf0f60d85f6e9d2
Janik von Rotz <login@janikvonrotz.ch>
2023-08-22 15:57:16 +0200

    [license_ocad] refactor methods

--- license_ocad/models/license_license.py
+++ license_ocad/models/license_license.py
@@ -72,11 +72,15 @@ def get_ocad2018_checksum(v, lnum, e, lname):
 class License(models.Model):
     _inherit = 'license.license'
 
+    # Existing fields
+    client_order_ref = fields.Char(required=True)
+
     # New fields
     company_id = fields.Many2one('res.company', string='Company', required=True, default=lambda self: self.env.company)
     download_token = fields.Char(compute='_compute_download_token', readonly=True, store=True, precompute=True)
     download_link = fields.Char(compute='_compute_download_link', readonly=True, store=True)
-    
+    registered = fields.Boolean(readonly=True)
+
     @api.depends('name')
     def _compute_download_token(self):
         char_table = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789abcdefghijklmnopqrstuvwxyz' # 58 char
@@ -105,24 +109,6 @@ class License(models.Model):
                 # Result: 5E27-8047-C507
                 license.key = ''.join(get_ocad2018_checksum(version, int(license.name), edition_long, license.client_order_ref))
 
-    def _enable_license(self):
-        message = ''
-        for license in self:
-
-            edition_short = license.product_id.get_value_by_key('EditionShort')
-
-            url = 'https://www.ocad.com/ocadintern/db_increaseCounter/increaseCounter_2018.php'
-            params = {
-                'edition': edition_short,
-                'licenseNumber': license.name,
-            }
-            auth = (self.company_id.ocad_username, self.company_id.ocad_password)
-
-            response = requests.post(url, params=params, auth=auth)
-            message += response.text + '\n'
-
-        return message
-
     def _create_license(self):
         message = ''
         for license in self:
@@ -150,12 +136,48 @@ class License(models.Model):
 
         return message
 
-    def action_activate(self):
-        """Create and enable license."""
-        super().action_activate()
+    def _enable_license(self):
+        message = ''
+        for license in self:
 
-        message = self._create_license()
-        message += self._enable_license()
+            edition_short = license.product_id.get_value_by_key('EditionShort')
+
+            url = 'https://www.ocad.com/ocadintern/db_increaseCounter/increaseCounter_2018.php'
+            params = {
+                'edition': edition_short,
+                'licenseNumber': license.name,
+            }
+            auth = (self.company_id.ocad_username, self.company_id.ocad_password)
+
+            response = requests.post(url, params=params, auth=auth)
+            message += response.text + '\n'
+
+        return message
+
+    def _disable_license(self):
+        message = ''
+        for license in self:
+
+            url = 'https://www.ocad.com/ocadintern/db_increaseCounter/deactivateActivation_2018.php'
+            params = {
+                'ProductKeyValue': '',
+                'LicenseNumberValue': license.name,
+                'StatusValue': 3,
+                'IdValue': ''
+            }
+            auth = (self.company_id.ocad_username, self.company_id.ocad_password)
+
+            response = requests.post(url, params=params, auth=auth)
+            message += response.text + '\n'
+
+        return message
+
+    def _get_action_notification(self, message):
+        notification_type = 'success'
+        notification_sticky = False
+        if 'FEHLER' in message or 'Unauthorized' in message:
+            notification_type = 'danger'
+            notification_sticky = True
 
         return {
             'type': 'ir.actions.client',
@@ -163,19 +185,43 @@ class License(models.Model):
             'params': {
                 'title': 'OCAD License Service',
                 'message': message,
-                'sticky': False,
-                'type': 'danger' if 'FEHLER' in message else 'success',
+                'sticky': notification_sticky,
+                'type': notification_type,
                 'next': {'type': 'ir.actions.act_window_close'},  # Refresh the form
             }
         }
+
+    def action_activate(self):
+        """Create and enable license."""
+        super().action_activate()
+
+        message = self._create_license()
+        # message += self._enable_license()
+
+        if not ('FEHLER' in message or 'Unauthorized' in message):
+            self.write({'registered': True})
+
+        return self._get_action_notification(message)
 
     # def action_reset(self):
     #     super().action_reset()
     #     for license in self:
 
-    # def action_disable(self):
-    #     super().action_disable()
-    #     for license in self:
+    def action_disable(self):
+        """Disable license."""
+        super().action_disable()
+
+        message = self._disable_license()
+
+        return self._get_action_notification(message)
+
+    def action_enable(self):
+        """Create and enable license."""
+        super().action_activate()
+
+        message = self._enable_license()
+
+        return self._get_action_notification(message)
 
     # def action_cancel(self):
     #     super().action_cancel()
