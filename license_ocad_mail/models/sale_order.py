4a04edb58121b846670f6b693716aed4c0bac198
Janik von Rotz <login@janikvonrotz.ch>
2025-01-13 17:58:45 +0100

    feat: update

--- license_ocad_mail/models/sale_order.py
+++ license_ocad_mail/models/sale_order.py
@@ -18,7 +18,7 @@ class SaleOrder(models.Model):
             if order.website_id and any(order.order_line.mapped("is_license")):
 
                 # Create licenses
-                order.order_line.license_exists()
+                order.order_line.update_license()
 
                 if not order.license_exists:
 
